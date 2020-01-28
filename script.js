c706dc2d5948d7ec3a512a19536e7c5056ed9840
cameron <cameronshaw95@gmail.com>
2020-01-28 20:57:42 +0900

    fixing problems

--- script.js
+++ script.js
@@ -39,7 +39,32 @@
 
 
   window.sr = ScrollReveal();
-  sr.reveal('.anim');
+  sr.reveal('.anim', { delay: 200 });
+
+
+//   var followMouse = function followMouse() {
+//     key = requestAnimationFrame(followMouse);
+
+//     if(!x || !y) {
+//       x = xmouse;
+//       y = ymouse;
+//     } else {
+//         dx = (xmouse - x) * 0.125;
+//         dy = (ymouse - y) * 0.125;
+//         if(Math.abs(dx) + Math.abs(dy) < 0.1) {
+//           x = xmouse;
+//           y = ymouse;
+//         } else {
+//             x += dx;
+//             y += dy;
+//       }
+//     }
+//     ball.style.left = x +'px';
+//     ball.style.top = y + 'px';
+//   };
+
+// document.getElementById("bodyLoad").onload = function() {followMouse()};
+
 
 
 })();
